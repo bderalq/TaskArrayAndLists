5a5ebbb273e65ad5bd4d9ad6af7fef9df4d1b51c
baderalqallaf <baderalqallaf@BADERALQALLE446>
2024-12-22 11:55:09 +0000

    bkefr

--- TaskArrayAndLists/Program.cs
+++ TaskArrayAndLists/Program.cs
@@ -18,22 +18,31 @@ groceries.Insert(2, "orange");
 // remove one element 
 groceries.Remove("cherry");
 
-// check if banana exists in the arraylist and print it 
+// check if an item exists in the arraylist and print it 
 
-if (groceries.Contains("banana"))
+Console.WriteLine("Enter item :");
+string checker = Console.ReadLine();
+
+if (groceries.Contains(checker))
 {
-    foreach (var item in groceries)
-    {
-        Console.WriteLine(item);
-    }
-    Console.WriteLine("banana exists in the list");
+    Console.WriteLine(checker + " exists in the list");
+} else { Console.WriteLine(checker + " does not exist in the list ");
 }
 
 Console.WriteLine("----------------------");
 
-// find the index of apple and print it 
+// find the index of an item and print it 
 
-Console.WriteLine("index of apple in the list is = " + groceries.IndexOf("apple"));
+Console.WriteLine("Check index of an item. Enter item :");
+string checktwo = Console.ReadLine();
+
+if (groceries.Contains(checktwo))
+{
+    Console.WriteLine("index of " + checktwo + " in the list is = " + groceries.IndexOf(checktwo));
+
+}
+else { Console.WriteLine(checktwo + "does not exist in the list."); 
+}
 
 Console.WriteLine("----------------------");
 
@@ -41,3 +50,21 @@ Console.WriteLine("----------------------");
 
 Console.WriteLine("number of elements in the array list is = " + groceries.Count);
 
+// bonus 
+
+ArrayList threshold = new ArrayList() {10, 20, 30, 40, 50, 60};
+
+Console.WriteLine("Enter new number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ArrayList newarray = new ArrayList();
+
+foreach (int elements in threshold) { 
+   
+    if (number > elements)
+    {
+        newarray.Add(number);
+        return;
+    }
+}
+foreach(var elements in newarray) { Console.WriteLine(elements); }    
